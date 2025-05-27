d137a07718d1d62129775b010c95bcbe8a8c6f37
itork <itork555@gmail.com>
2025-05-27 16:29:17 +0200

    chore: update project dependencies and configuration files

--- types/enviroment.d.ts
+++ types/enviroment.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
     SUPABASE_SERVICE_KEY: string;
+    GOOGLE_CLIENT: string;
+    GOOGLE_SECRET: string;
   }
 }
